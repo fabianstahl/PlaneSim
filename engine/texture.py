88543ebf395a06ee22dbeba03a96fca027fa3dc7
Fabian Stahl <fabian.stahl1@web.de>
2025-06-09 22:24:23 +0200

    swapped cv2 for Pillow as image import is faster

--- engine/texture.py
+++ engine/texture.py
@@ -1,5 +1,7 @@
 from OpenGL.GL import *
-from PIL import Image
+import os
+import cv2
+import numpy as np
 
 class Texture():
 
@@ -13,12 +15,28 @@ class Texture():
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
-        try: 
-            image = Image.open(path).convert("RGBA").transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-        except:
-            image = Image.open(backup_path).transpose(Image.Transpose.FLIP_TOP_BOTTOM)
-        img_data = image.convert("RGBA").tobytes()
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        
+        try:
+            if not os.path.exists(path):
+                raise FileNotFoundError
+            image = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+            if image is None:
+                raise ImportError
+        except: 
+            image = cv2.imread(backup_path, cv2.IMREAD_UNCHANGED)
+
+        # Flip vertically to match OpenGL's coordinate system
+        image = cv2.flip(image, 0)
+
+        # Add full alpha channel if missing
+        if image.shape[2] == 3:
+            alpha = np.full((image.shape[0], image.shape[1], 1), 255, dtype=np.uint8)
+            image = np.concatenate((image, alpha), axis=2)
+
+        # Convert BGR(A) to RGB(A)
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2RGBA)
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.shape[1], image.shape[0], 0, GL_RGBA, GL_UNSIGNED_BYTE, image.tobytes())
         glGenerateMipmap(GL_TEXTURE_2D)
 
     def use(self):
