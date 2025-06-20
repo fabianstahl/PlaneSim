5d264e740104afbd127bb6f146008740037e238f
Fabian Stahl <fabian.stahl1@web.de>
2025-06-20 16:58:40 +0200

    code cleanuo 'shader' module

--- engine/shader.py
+++ engine/shader.py
@@ -1,20 +1,24 @@
 from OpenGL.GL import *
 
-def load_shader_from_file(path):
+def load_shader_from_file(path: str) -> str:
     with open(path, 'r') as file:
         return file.read()
 
 
 class Shader():
 
-    def __init__(self, source_path, shader_type):
+    def __init__(self, source_path: str, shader_type: str):
         shader = glCreateShader(shader_type)
         glShaderSource(shader, load_shader_from_file(source_path))
         glCompileShader(shader)
         if not glGetShaderiv(shader, GL_COMPILE_STATUS):
             print("Shader program linking failed:")
             print(glGetShaderInfoLog(shader).decode())
-        self.shader = shader
+        self._shader = shader
+
+    @property
+    def shader(self) -> int:
+        return self._shader
 
     def release(self):
         glDeleteShader(self.shader)
@@ -22,38 +26,38 @@ class Shader():
 
 class Program():
 
-    def __init__(self, vertex_shader, fragment_shader):
+    def __init__(self, vertex_shader: Shader, fragment_shader: Shader):
 
-        self.vertex_shader      = vertex_shader
-        self.fragment_shader    = fragment_shader
+        self._vertex_shader     = vertex_shader
+        self._fragment_shader   = fragment_shader
 
-        self.shader_program     = glCreateProgram()
-        glAttachShader(self.shader_program, vertex_shader.shader)
-        glAttachShader(self.shader_program, fragment_shader.shader)
-        glLinkProgram(self.shader_program)
+        self._shader_program     = glCreateProgram()
+        glAttachShader(self._shader_program, vertex_shader.shader)
+        glAttachShader(self._shader_program, fragment_shader.shader)
+        glLinkProgram(self._shader_program)
 
         # Check for linking errors
-        if not glGetProgramiv(self.shader_program, GL_LINK_STATUS):
+        if not glGetProgramiv(self._shader_program, GL_LINK_STATUS):
             print("Shader program linking failed:")
-            print(glGetProgramInfoLog(self.shader_program).decode())
+            print(glGetProgramInfoLog(self._shader_program).decode())
 
         """
         # Parint uniform IDs
-        num_uniforms = glGetProgramiv(self.shader_program, GL_ACTIVE_UNIFORMS)
+        num_uniforms = glGetProgramiv(self._shader_program, GL_ACTIVE_UNIFORMS)
         print("Active uniforms:")
         for i in range(num_uniforms):
-            name, size, type_ = glGetActiveUniform(self.shader_program, i)
+            name, size, type_ = glGetActiveUniform(self._shader_program, i)
             print(f"  {i}: {name}")
         """
 
     def use(self):
-        glUseProgram(self.shader_program)
+        glUseProgram(self._shader_program)
 
-    def get_uniform_location(self, identifier):
-        glUseProgram(self.shader_program)
-        return glGetUniformLocation(self.shader_program, identifier)
+    def get_uniform_location(self, identifier: str) -> int:
+        glUseProgram(self._shader_program)
+        return glGetUniformLocation(self._shader_program, identifier)
     
     def release(self):
-        self.vertex_shader.release()
-        self.fragment_shader.release()
-        glDeleteProgram(self.shader_program)
+        self._vertex_shader.release()
+        self._fragment_shader.release()
+        glDeleteProgram(self._shader_program)
