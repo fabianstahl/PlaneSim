a7c9c114bb6de27cb5937e971fd79e2dccb76591
Fabian Stahl <fabian.stahl1@web.de>
2025-06-20 17:27:35 +0200

    code cleanup 'geography' module

--- geography.py
+++ geography.py
@@ -1,11 +1,13 @@
 import csv
 import utils
 import numpy as np
+import configparser as cfg
+from typing import Tuple, List
 
 
 class Airport:
 
-    def __init__(self, name, position, country, type):
+    def __init__(self, name: str, position: Tuple[float, float], country: str, type: str):
         self.name       = name
         self.position   = position
         self.country    = country
@@ -14,18 +16,16 @@ class Airport:
 
 class AirportManager:
 
-    def __init__(self, airport_csv, filter_types=["large_airport"]):
+    def __init__(self, airport_csv: str, filter_types: List[str] = ["large_airport"]):
         
         # Parse all airports
-        self.airports   = []
+        file    = open(airport_csv, "r")
+        reader  = csv.reader(file)
+        columns = next(reader)      # Skip header
 
-        file            = open(airport_csv, "r")
-        reader          = csv.reader(file)
-
-        # Skip header
-        columns         = next(reader)
         #print("\n".join(["{}: {}".format(i, c) for i, c in enumerate(columns)]))
 
+        self.airports   = []
         for line in reader:
             airport_type = line[2]
             if not airport_type in filter_types:
@@ -37,11 +37,10 @@ class AirportManager:
         print("Found '{}' airports!".format(len(self.airports)))
 
 
-    def position_by_name(self, substring):
+    def position_by_name(self, substring: str) -> Tuple[float, float]:
         for airport in self.airports:
             if substring.lower() in airport.name.lower():
                 return airport.position
-
 
 
 
@@ -52,7 +51,7 @@ class Mission:
         self.threshold_distance = threshold_distance
 
 
-    def check_distance(self, pos):
+    def check_distance(self, pos: Tuple[float, float]):
         #print(pos, self.target.position)
         x1, y1  = pos
         x2, y2  = self.target.position
@@ -60,18 +59,21 @@ class Mission:
         return dist < self.threshold_distance
 
 
+
 class MissionManager:
 
-    def __init__(self, configs):
+    def __init__(self, configs: cfg.SectionProxy):
         
         self.configs            = configs
         self.airport_manager    = AirportManager(configs.get("airport_file"))
         self.mission            = None
 
-    def get_airports(self):
+
+    def get_airports(self) -> List[Airport]:
         return self.airport_manager.airports
     
-    def new_mission(self):
+
+    def new_mission(self) -> Mission:
         rand_index              = np.random.randint(len(self.airport_manager.airports))
         self.mission            = Mission(self.airport_manager.airports[rand_index], self.configs.getfloat("target_radius"))
         return self.mission
